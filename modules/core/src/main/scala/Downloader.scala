de3309d1623d8f94ad85b58c61b0dc5d7de233b8
Anton Sviridov <keynmol@gmail.com>
2023-04-21 21:30:30 +0100

    fix CI

--- modules/core/src/main/scala/Downloader.scala
+++ modules/core/src/main/scala/Downloader.scala
@@ -1,15 +1,32 @@
+/*
+ * Copyright 2023 Anton Sviridov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.indoorvivants.yank
 
 import java.io._
-import scala.util.Try
 import java.net.URL
+
+import scala.util.Try
 
 trait Downloader {
   def download(link: String, to: java.nio.file.Path): Try[File]
 }
 
 object Downloader {
-  def basic(reporter: ProgressReporter, bufSize: Int = 102_400): Downloader =
+  def basic(reporter: ProgressReporter, bufSize: Int = 102400): Downloader =
     new Basic(
       bufSize,
       reporter
